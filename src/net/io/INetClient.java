1acf8914619288acf173ed9451b1506c51f15e5e
hyfu <71806228>
2015-12-23 16:49:45 +0800

    m

--- src/net/io/INetClient.java
+++ src/net/io/INetClient.java
@@ -1,26 +1,9 @@
 package net.io;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-
-import net.io.message.IMessage;
-
 public interface INetClient {
 	
 	void connect(byte[] data);
 	
 	void close() throws Exception;
-	
-	default void sendMessage(IMessage message) throws Exception {
-		ByteArrayOutputStream bout = new ByteArrayOutputStream();
-		DataOutputStream dos = new DataOutputStream(bout);
-		dos.writeInt(message.getMessageId());
-		byte[] datas = message.getByteArray();
-		dos.writeInt(datas.length);
-//		dos.write(EncryptUtil.encrypt(resultMessage, resultMessage.length, EncryptUtil.PASSWORD));
-		dos.write(datas);
-		byte[] bytes = bout.toByteArray();
-		connect(bytes);
-	}
 
 }
