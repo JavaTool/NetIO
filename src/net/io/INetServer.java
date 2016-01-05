1a75d63a5f92320dd15609340d370dc2f8b8e364
hyfu <71806228>
2016-01-05 20:06:25 +0800

    m

--- src/net/io/INetServer.java
+++ src/net/io/INetServer.java
@@ -1,11 +1,24 @@
 package net.io;
 
+/**
+ * A server of net connect.
+ * @author 	fuhuiyuan
+ */
 public interface INetServer {
 	
+	/**
+	 * Binding an ip and port, start this server.
+	 * @throws 	Exception
+	 */
 	void bind() throws Exception;
-	
+	/**
+	 * Stop server.
+	 */
 	void shutdown();
-	
+	/**
+	 * The port of this server.
+	 * @return	port of this server
+	 */
 	int getPort();
 
 }
